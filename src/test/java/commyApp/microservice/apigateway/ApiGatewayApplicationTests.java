e008d445aa1215b293cb71a0e0f4e50f0049befe
Sudeepta66 <sudeepta66@gmail.com>
2022-06-05 12:42:18 +0530

    api-gateway

---
+++ src/test/java/commyApp/microservice/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package commyApp.microservice.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
